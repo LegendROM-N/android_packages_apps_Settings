58bdb0dc2a71f4e04d516a54c98d9592f3d46efa
beanstown106 <nbenis106@gmail.com>
2016-01-19 18:48:02 +0000

    Longpress power lockscreen torch [2/2]
    DU edits :
    - Clean up a bit, remove unused imports
    - Remove switch preference on the Nexus 7 (flo)
    
    Change-Id: I10c8aa06c81ca020967d5d91e169a4531ed8a06a
    
    LegendROM:
    - Add custom Utils

--- src/com/android/settings/Legend/tabs/LockScreen.java
+++ src/com/android/settings/Legend/tabs/LockScreen.java
@@ -2,30 +2,49 @@ package com.android.settings.Legend.tabs;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-
+import com.android.settings.utils.Utils;
+import com.android.settings.cyanogenmod.SystemSettingSwitchPreference;
 
 public class LockScreen extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockScreen";
 
+    private static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
+
+    private SwitchPreference mKeyguardTorch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.lockscreen);
+	PreferenceScreen prefSet = getPreferenceScreen();
+
+	ContentResolver resolver = getActivity().getContentResolver();
+
+	mKeyguardTorch = (SwitchPreference) findPreference(KEYGUARD_TOGGLE_TORCH);
+        mKeyguardTorch.setOnPreferenceChangeListener(this);
+        if (!Utils.deviceSupportsFlashLight(getActivity())) {
+            prefSet.removePreference(mKeyguardTorch);
+        } else {
+        mKeyguardTorch.setChecked((Settings.System.getInt(resolver,
+                Settings.System.KEYGUARD_TOGGLE_TORCH, 0) == 1));
+        }
     }
 
     @Override
@@ -44,7 +63,12 @@ public class LockScreen extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
+        if  (preference == mKeyguardTorch) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.KEYGUARD_TOGGLE_TORCH, checked ? 1:0);
+            return true;
+        }
+        return false;
     }
 }
