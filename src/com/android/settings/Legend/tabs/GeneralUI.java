ebf12af4035ff81a9687bf2f839c736eef72e7f4
cristianomatos <cristianomatos@gmail.com>
2017-01-08 08:00:31 +0100

    FAB Recents Clear All [2/2]
    *DU Edits*
    - Off by default
    
    Change-Id: If7b33e315808047a8b03a20d3b96dbb47db07c63

--- src/com/android/settings/Legend/tabs/GeneralUI.java
+++ src/com/android/settings/Legend/tabs/GeneralUI.java
@@ -24,8 +24,10 @@ public class GeneralUI extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "GeneralUI";
     private static final String SCREENSHOT_DELAY = "screenshot_delay";
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
     private SeekBarPreference mScreenshotDelay;
+    private ListPreference mRecentsClearAllLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,14 @@ public class GeneralUI extends SettingsPreferenceFragment implements
                 Settings.System.SCREENSHOT_DELAY, 100);
         mScreenshotDelay.setValue(screenshotDelay / 1);
         mScreenshotDelay.setOnPreferenceChangeListener(this);
+
+	// clear all recents
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -57,11 +67,19 @@ public class GeneralUI extends SettingsPreferenceFragment implements
         super.onPause();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mScreenshotDelay) {
-            int screenshotDelay = (Integer) objValue;
+            int screenshotDelay = (Integer) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+	} else if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) newValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
             return true;
         }
 	return false;
