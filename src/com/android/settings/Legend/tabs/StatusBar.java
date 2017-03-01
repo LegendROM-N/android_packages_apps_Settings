2528bdd1599a77aa1e82ee162cd2dc2f8e145078
maxwen <max.weninger@gmail.com>
2017-03-01 14:19:02 +0530

    Settings: add OmniJaws config
    Change-Id: Idea7a4d2471e7524671fabd731091b0463d9597f
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Remove service settings duplicate
    
    Change-Id: I770881349c539bf3c972262fbbf912c2fc02d09e

--- src/com/android/settings/Legend/tabs/StatusBar.java
+++ src/com/android/settings/Legend/tabs/StatusBar.java
@@ -49,6 +49,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String PREF_COLUMNS = "qs_layout_columns";
     private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+    private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
+    private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
+
+    private ListPreference mStatusBarWeather;
 
     private ListPreference mSmartPulldown;
     private SwitchPreference mForceExpanded;
@@ -166,6 +171,18 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mRowsLandscape.setValue(rowsLandscape / 1);
         mRowsLandscape.setOnPreferenceChangeListener(this);
 
+	// Status bar weather
+        mStatusBarWeather = (ListPreference) findPreference(PREF_STATUS_BAR_WEATHER);
+        int temperatureShow = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        mStatusBarWeather.setValue(String.valueOf(temperatureShow));
+        if (temperatureShow == 0) {
+            mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+        } else {
+            mStatusBarWeather.setSummary(mStatusBarWeather.getEntry());
+        }
+        mStatusBarWeather.setOnPreferenceChangeListener(this); 
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -230,9 +247,82 @@ public class StatusBar extends SettingsPreferenceFragment implements
             mHeaderProvider.setSummary(mHeaderProvider.getEntries()[valueIndex]);
             mDaylightHeaderPack.setEnabled(value.equals(mDaylightHeaderProvider));
             return true;
-        }
+	 } else if (preference == mStatusBarWeather) {
+            int temperatureShow = Integer.valueOf((String) objValue);
+            int valueIndex = mStatusBarWeather.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP,
+                    temperatureShow, UserHandle.USER_CURRENT);
+            if (temperatureShow == 0) {
+                mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+            } else {
+                mStatusBarWeather.setSummary(
+                        mStatusBarWeather.getEntries()[valueIndex]);
+            }
+            return true;
+         }
 	return false;
     }
+
+    private boolean isOmniJawsServiceInstalled() {
+        return PackageUtils.isAvailableApp(WEATHER_SERVICE_PACKAGE, getActivity());
+    }
+
+    private void getAvailableWeatherIconPacks(List<String> entries, List<String> values) {
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.WeatherIconPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            Log.d("maxwen", packageName);
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                values.add(0, r.activityInfo.name);
+            } else {
+                values.add(r.activityInfo.name);
+            }
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                entries.add(0, label);
+            } else {
+                entries.add(label);
+            }
+        }
+        i = new Intent(Intent.ACTION_MAIN);
+        i.addCategory(CHRONUS_ICON_PACK_INTENT);
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            values.add(packageName + ".weather");
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            entries.add(label);
+        }
+     }
+ 
+     private boolean isOmniJawsEnabled() {
+         final Uri SETTINGS_URI
+             = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
+ 
+         final String[] SETTINGS_PROJECTION = new String[] {
+             "enabled"
+         };
+ 
+         final Cursor c = getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                 null, null, null);
+         if (c != null) {
+             int count = c.getCount();
+             if (count == 1) {
+                 c.moveToPosition(0);
+                 boolean enabled = c.getInt(0) == 1;
+                 return enabled;
+             }
+         }
+         return true;
+     }
 
     private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
         Intent i = new Intent();
